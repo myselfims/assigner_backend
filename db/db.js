5bb353dd629d548d6f45211279a959d8f604b48b
myselfims <shaikhimran7585@gmail.com>
2023-10-22 12:09:40 +0530

    poll option added to database

--- db/db.js
+++ db/db.js
@@ -2,7 +2,14 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize('railway', 'root', 'ykmXliGSDNKnh3oSlpTH', {
     host: 'containers-us-west-202.railway.app',
-    dialect: 'mysql' 
+    dialect: 'mysql',
+    pool: {
+        max: 15,
+        min: 5,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000
+      }, 
 });
 
 // Assuming this code is inside an async function or using .then() to handle promises
